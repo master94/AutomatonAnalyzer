d18b1f0cfb1fdd364957ba369bb1057f3d3b159b
Valery Shevchuk <shevchuk.valery@gmail.com>
2015-11-12 23:34:46 +0200

    Add two additional tests
    One of them for corner case, when there are only two states.

--- test_data.py
+++ test_data.py
@@ -12,5 +12,19 @@ at1 = AutomatonTest(
       )
 
 
+at2 = AutomatonTest(
+	automaton = Automaton('ab*cd*ef* - automaton', 4, { 0: {'a': 1}, 1: {'b': 1, 'c': 2}, 2: {'d': 2, 'e': 3}, 3: {'f': 3} }, 0, 3),
+        valid_strings = ['ace', 'abce', 'abbbbbbbce', 'acddddddddde', 'aceffffffffff', 'abbbbbcddddddddefffffffff', 'abcdef'],
+        invalid_strings = ['aa', 'aaaa', 'a', '', 'cc', 'bb', 'aabb', 'aacc', 'abcdf']
+      )
+
+
+at3 = AutomatonTest(
+	automaton = Automaton('a*bc* - automaton', 2, { 0: {'a': 0, 'b': 1}, 1: {'c': 1} }, 0, 1),
+        valid_strings = ['b', 'ab', 'aab', 'aaaaaaaaaab', 'bc', 'bcc', 'bccccccccccccc', 'abc', 'aabcc', 'aaaaaaaabcccccccc' ],
+        invalid_strings = ['3453434', 'a', 'c', 'ac', 'bb', 'abb', 'aaaaccccc', 'abcdf']
+      )
+
+
 def get_test_data():
-    return [at1]
+    return [at1, at2, at3]
